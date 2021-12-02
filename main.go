8c23c311c043adb1b2a7ae9ae5c8113f1ae29283
Soliquidus <tibo.pfeifer@gmail.com>
2021-12-02 09:52:26 +0100

    Job and result dispatch added with channel management

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"imgproc/filter"
 	"imgproc/task"
+	"time"
 )
 
 func main() {
 	var f filter.Filter = filter.Grayscale{}
-	t := task.NewWaitGrpTask("./img", "output", f)
+	//t := task.NewWaitGrpTask("./img", "output", f)
+	t := task.NewChanTask("./img", "output", f, 16)
+
+	start := time.Now()
 	err := t.Process()
 	if err != nil {
-		return
+		fmt.Printf("Error while processing, %v", err)
 	}
-
+	elapsed := time.Since(start)
+	fmt.Printf("Image processing took %s\n", elapsed)
 }
