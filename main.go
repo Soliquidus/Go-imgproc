82434000f36d14b3a2c5791c5c925a14aed6a619
Soliquidus <tibo.pfeifer@gmail.com>
2021-12-02 10:23:37 +0100

    CLI commands added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"imgproc/filter"
 	"imgproc/task"
@@ -8,9 +9,29 @@ import (
 )
 
 func main() {
-	var f filter.Filter = filter.Grayscale{}
-	//t := task.NewWaitGrpTask("./img", "output", f)
-	t := task.NewChanTask("./img", "output", f, 16)
+	// CLI commands
+	var srcDir = flag.String("src", "", "Input directory")
+	var dstDir = flag.String("dst", "", "Output directory")
+	var filterType = flag.String("filter", "grayscale", "grayscale/blur")
+	var taskType = flag.String("task", "waitgrp", "waitgrp/channel")
+	var poolSize = flag.Int("poolsize", 4, "Workers pool size for the channel task")
+	flag.Parse()
+
+	var f filter.Filter
+	switch *filterType {
+	case "grayscale":
+		f = filter.Grayscale{}
+	case "blur":
+		f = filter.Blur{}
+	}
+
+	var t task.Tasker
+	switch *taskType {
+	case "waitgrp":
+		t = task.NewWaitGrpTask(*srcDir, *dstDir, f)
+	case "channel":
+		t = task.NewChanTask(*srcDir, *dstDir, f, *poolSize)
+	}
 
 	start := time.Now()
 	err := t.Process()
